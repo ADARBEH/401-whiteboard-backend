0d8f947c762b4bcb652fa2c91a51e8463f2bca04
ADARBEH <retaad99@gmail.com>
2022-09-12 19:01:34 +0000

    f

--- models/index.js
+++ models/index.js
@@ -4,17 +4,21 @@ const Post = require('./post.model.js');
 const POSTGRES_URL = process.env.DB_DATABASE || "postgresql://adarbeh:0000@localhost:5432/adarbeh";
 
 
-const sequelizeOptions = {
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-};
+// const sequelizeOptions = {
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// };
 
 
-const sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
+const sequelize = new Sequelize(POSTGRES_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {}, 
+  });
 
 module.exports = {
     db: sequelize,
