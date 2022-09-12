f7d59a76aea7d9b66b7ca2d1bef17921435fcc20
ADARBEH <retaad99@gmail.com>
2022-09-12 18:19:20 +0000

    d

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const {db} = require('./models/index.js');
 
 
 db.sync().then(() => {
-    const port = process.env.PORT || 3001;
-    server.start(port);
+    server.start(process.env.PORT);
 }
 ).catch((err) => {
     console.log(err);
