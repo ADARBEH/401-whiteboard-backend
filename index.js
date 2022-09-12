454e7f162f383929b93359fc0ebec244b30c731e
ADARBEH <retaad99@gmail.com>
2022-09-12 18:31:24 +0000

    f

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const server = require('./server.js');
 require('dotenv').config();
 const {db} = require('./models/index.js');
 
+const PORT = process.env.PORT || 3000;
 
 db.sync().then(() => {
-    server.start(process.env.PORT);
+    server.start(PORT);
 }
 ).catch((err) => {
     console.log(err);
