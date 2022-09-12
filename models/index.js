375b7a3129c3707094e3f3976c70a50b1d174e88
ADARBEH <retaad99@gmail.com>
2022-09-12 22:19:18 +0000

    d

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const {Sequelize , DataTypes} = require('sequelize');
 const Post = require('./post.model.js');
-const POSTGRES_URL = process.env.DB_DATABASE || "postgresql://adarbeh:0000@localhost:5432/adarbeh";
+const POSTGRES_URL = process.env.DATABASE_URL || "postgresql://adarbeh:0000@localhost:5432/adarbeh";
 
 
 // const sequelizeOptions = {
