f91e0dd8ce80cdf63b7a3c883a69f32f91538f75
ADARBEH <retaad99@gmail.com>
2022-10-05 20:09:00 +0000

    new

--- models/index.js
+++ models/index.js
@@ -3,9 +3,9 @@ const { Sequelize, DataTypes } = require('sequelize');
 const Post = require('./post.model.js');
 const Comment = require('./comment.model.js');
 const user = require('./user.model.js');
-// const POSTGRES_URL = "postgres://wgszegnyqxpvex:faad326b6b4c9dbb79fe34fcd5e5d0e195e88574d6c44530c47a242ce146d335@ec2-3-93-206-109.compute-1.amazonaws.com:5432/d20ko6bh6475hk";
+const POSTGRES_URL = "postgres://wgszegnyqxpvex:faad326b6b4c9dbb79fe34fcd5e5d0e195e88574d6c44530c47a242ce146d335@ec2-3-93-206-109.compute-1.amazonaws.com:5432/d20ko6bh6475hk";
 
-const POSTGRES_URL = "postgres://adarbeh:0000@localhost:5432/adarbeh";
+// const POSTGRES_URL = "postgres://adarbeh:0000@localhost:5432/adarbeh";
 const user_comment_routes = require('../collections/user_comment_routes.js');
 
 const sequelizeOptions = {
@@ -24,7 +24,7 @@ const sequelizeOptions = {
 //     dialectOptions: {},
 // }
 
-const sequelize = new Sequelize(POSTGRES_URL, {});
+const sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
 
 const userModel = user(sequelize, DataTypes);
 const postModel = Post(sequelize, DataTypes);
